838bc46d51e7ca7e93531f9e2a6f290b3c8088b0
Your Name <karinepestana>
2017-06-10 18:08:16 -0300

    Iniciando parte 2; Makefile pronto; Erro de segmentação

---
+++ C+Fortran/CeFortran.c
@@ -0,0 +1,29 @@
+#include <stdlib.h> 
+#include <stdio.h>
+
+#define TAM 100
+
+extern int* insertionsort(int *sorted, int *tam) ;
+void ExibeVetor(int* vet, int n);
+ 
+int main(int argc, char **argv)
+{
+    int *sorted, i, *tam;
+ 
+    sorted = malloc(sizeof(int)*TAM);
+ 
+    for(i=0; i<TAM; i++){
+        sorted[i] = rand()% 1000;
+    }
+    printf("Vai ordenar o vetor\n");
+    *tam = 100;
+    sorted = insertionsort(sorted, tam);
+    ExibeVetor(sorted, TAM);
+    return 0;
+}
+ 
+void ExibeVetor(int* vet, int n){
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", vet[i]);
+}
---
+++ C+Fortran/insertionsortFortran.f90
@@ -0,0 +1,24 @@
+SUBROUTINE insertionsort(sorted, tam)   
+   
+   INTEGER, INTENT(IN) :: tam
+   INTEGER, POINTER, DIMENSION(:), INTENT(INOUT) :: sorted
+   !INTEGER, INTENT(OUT):: sorted(100)
+   
+   INTEGER i, k, aux
+   i = 0
+   k = 0
+   aux = 0
+
+   print *, "Ordenando Fortran"
+
+
+    DO i = 0, SIZE(sorted)-1
+       k = MINLOC(sorted(i:), 1) + i - 1
+       IF (sorted(i) > sorted(k)) THEN
+          aux = sorted(i)
+          sorted(i) = sorted(k)
+          sorted(k) = aux
+       END IF
+    END DO  
+
+END SUBROUTINE insertionsort
